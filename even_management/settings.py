972ad80e4683677524cff49e21be0ec56f9587c4
ARAFAT <mdarafathossen.py@gmail.com>
2025-11-10 22:08:55 +0600

    online Datebase add

--- even_management/settings.py
+++ even_management/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,15 +78,23 @@ WSGI_APPLICATION = 'even_management.wsgi.application'
 #    }
 #}
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_management',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_management',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_management_db_nrc0_user:mTJFJoLs8KDSkVVYqDo0rlWaP5lBhG8T@dpg-d490mi49c44c73bdsd80-a.oregon-postgres.render.com/event_management_db_nrc0',
+        conn_max_age=600
+    )
 }
 
 
