cb87d6e7289cff2cd76554e24e2d752770407172
ARAFAT <mdarafathossen.py@gmail.com>
2025-11-10 21:25:53 +0600

    settings update for online host

--- even_management/settings.py
+++ even_management/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = 'django-insecure-d#$o0xlu7fjvp)^)w#w=pjq&)u_@=wsgr@n00!_$di2oy&c3qq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
